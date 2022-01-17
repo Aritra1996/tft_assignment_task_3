1b41fea2495f479ce5f3936b8da5d51d2a072347
Aritra1996 <arakshit.engineer@gmail.com>
2022-01-17 12:12:56 +0530

    Added multiple operands

--- src/App.js
+++ src/App.js
@@ -6,57 +6,37 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 0, operation: null, val1: null, displayValue: ''};
+    this.state = { value: '', newStart: false};
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(value) {
-    if(value==='+' || value==='-' || value==='*' || value==='/') {
-      if(this.state.operation===null) {
-        this.setState({val1: this.state.value, operation: value,
-        value:0, displayValue: this.state.displayValue+value});      
-      } else {
-        console.log('Before operation :-')
-        console.log('this.state.displayValue: '+this.state.displayValue)
-        console.log('value: '+value)
-        console.log('this.state.val1: '+this.state.val1)
-        console.log('this.state.value: '+this.state.value)
-        let latestValue;
-        if(this.state.operation==='+') {
-          latestValue = this.state.val1+this.state.value
-        } else if(this.state.operation==='-') {
-          latestValue = this.state.val1-this.state.value
-        } else if(this.state.operation==='*') {
-          latestValue = this.state.val1*this.state.value
-        } else if(this.state.operation==='/') {
-          latestValue = parseFloat((this.state.val1/this.state.value).toFixed(4))
+    if(value==='=') {
+      try { 
+        this.setState({value: eval(this.state.value), newStart: true})
+      } catch(e) {
+        if(e instanceof SyntaxError) {
+          alert('Invalid expression')
+        } else {
+          alert('Error occured')
+          console.log(e)
         }
-        console.log('After operation :-')
-        console.log('latestValue: '+latestValue)
-        console.log('this.state.displayValue: '+this.state.displayValue)
-        console.log('value: '+value)
-        console.log('this.state.val1: '+this.state.val1)
-        console.log('this.state.value: '+this.state.value)
-        this.setState({displayValue: latestValue + value, 
-          value: 0, operation: value, val1: latestValue})  
       }
-    } else if(value==='=') {
-      let latestValue;
-      if(this.state.operation==='+') {
-        latestValue = this.state.val1+this.state.value
-      } else if(this.state.operation==='-') {
-        latestValue = this.state.val1-this.state.value
-      } else if(this.state.operation==='*') {
-        latestValue = this.state.val1*this.state.value
-      } else if(this.state.operation==='/') {
-        latestValue = parseFloat((this.state.val1/this.state.value).toFixed(4))
-      }
-      this.setState({displayValue: latestValue.toString(), value: latestValue, operation: null})
+      
     } else if(value==='CE') {
-      this.setState({displayValue: '', value: 0, operation: null})
+      let newValue;
+      if(this.state.value.length>1) {
+        newValue = this.state.value.substring(0, this.state.value.length-1)
+        this.setState({value: newValue})
+      } else {
+        this.setState({value: ''})
+      }
     } else {
-      const latestValue = this.state.value*10 + parseInt(value)
-      this.setState({value: latestValue, displayValue: this.state.displayValue + value.toString()});
+      if(this.state.newStart) {
+        this.setState({ value: value, newStart: false})
+      } else {
+        this.setState({value: this.state.value + value});
+      }
     }
   }
 
@@ -64,7 +44,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div><h1>Calculator</h1></div>
-        <InputArea value={this.state.displayValue}></InputArea>
+        <InputArea value={this.state.value}></InputArea>
         <Buttons handleClick={this.handleClick}></Buttons>
       </div>
     );
--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,9 +1,13 @@
 import './InputArea.css'
 
 export function InputArea(props) {
+    let value = props.value
+    if(props.value==='') {
+        value = 0
+    }
     return(
         <div className="InputArea">
-            <input value={props.value} readOnly/>
+            <input value={value} readOnly/>
         </div>
     )
 }
