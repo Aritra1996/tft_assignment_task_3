0855be8472aeb56538952adfd899706c9e39fd29
Aritra1996 <arakshit.engineer@gmail.com>
2022-01-20 16:52:00 +0530

    Refactored codebase

--- src/App.js
+++ src/App.js
@@ -11,47 +11,7 @@ class App extends React.Component {
   }
 
   handleClick(value) {
-    if(value==='=') {
-      try { 
-        let expression = "return " + this.state.value
-        this.setState({value: Function(expression)(), newStart: true, operatorAtLast: false})
-      } catch(e) {
-        console.log(e)
-        if(e instanceof SyntaxError) {
-          alert('Invalid expression')
-        } else {
-          alert('Error occured')
-        }
-      }
-      
-    } else if(value==='CE') {
-      let newValue;
-      if(this.state.value.length>1) {
-        newValue = this.state.value.substring(0, this.state.value.length-1)
-        this.setState({value: newValue})
-      } else {
-        this.setState({value: ''})
-      }
-    } else {
-      if(this.state.newStart) {
-        if(value==='+'||value==='-'||value==='*'||value==='/') {
-          this.setState({value: this.state.value + value, newStart: false, operatorAtLast: true});
-        } else {
-          this.setState({ value: value, newStart: false})
-        }
-      } else {
-        if(value==='+'||value==='-'||value==='*'||value==='/') {
-          if(this.state.operatorAtLast) {
-            let newValue = this.state.value.substring(0, this.state.value.length-1) + value
-            this.setState({value: newValue, operatorAtLast: true});
-          } else {
-            this.setState({value: this.state.value + value, operatorAtLast: true});
-          }
-        } else {
-          this.setState({value: this.state.value + value, operatorAtLast: false});
-        }
-      }
-    }
+    this.setState({value: value})
   }
 
   render() {
@@ -59,7 +19,7 @@ class App extends React.Component {
       <div className="App">
         <div><h1>Calculator</h1></div>
         <InputArea value={this.state.value}></InputArea>
-        <Buttons handleClick={this.handleClick}></Buttons>
+        <Buttons handleClick={this.handleClick} exp={this.state.value}></Buttons>
       </div>
     );
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,33 +5,70 @@ export class Buttons extends React.Component {
 
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { lastEval : false, lastOperator: false};
+        this.handleOperands = this.handleOperands.bind(this)
+        this.handleOperations = this.handleOperations.bind(this)
+        this.handleOperators = this.handleOperators.bind(this)
 
     }
 
-    handleClick(e) {
-        this.props.handleClick(e.target.value);
+    handleOperands(e) {
+        if(this.state.lastEval) {
+            this.props.handleClick(e.target.value)
+        } else {
+            this.props.handleClick(this.props.exp + e.target.value)
+        }
+        this.setState({lastEval: false, lastOperator: false})
+    }
+
+    handleOperators(e) {
+        if(this.state.lastEval || this.props.exp==='') {
+           if(e.target.value==='+' || e.target.value==='-') {
+            this.props.handleClick(e.target.value)
+           }
+        } else if(this.state.lastOperator) {
+            this.props.handleClick(this.props.exp.substring(0, this.props.exp.length-1) + e.target.value)
+        } else {
+            this.props.handleClick(this.props.exp + e.target.value)
+        }
+        this.setState({lastEval: false, lastOperator: true})
+    }
+
+    handleOperations(e) {
+        if(e.target.value === 'CE') {
+            if(this.state.lastEval) {
+                this.props.handleClick('')
+            } else {
+                this.props.handleClick(this.props.exp.substring(0, this.props.exp.length-1))
+            }
+            this.setState({lastEval: false, lastOperator: false})
+        } else if (e.target.value === '=') {
+            let expression = "return " + this.props.exp
+            this.props.handleClick(Function(expression)())
+            this.setState({lastEval: true, lastOperator: false})
+        }
     }
 
     render() {
+        let operators = ['+', '-', '*', '/']
+        let operands = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        let operations = ['CE', '=']
+
+        let items = operators.map((operator) =>
+            <div key={operator}><input type='button' value={operator} onClick={this.handleOperators}/></div>
+        )
+
+        items = items.concat(operands.map((operand) =>
+            <div key={operand}><input type='button' value={operand} onClick={this.handleOperands}/></div>
+        ))
+
+        items = items.concat(operations.map((operation) =>
+            <div key={operation}><input type='button' value={operation} onClick={this.handleOperations}/></div>
+        ))
+        
         return(
             <div className="Numbers">
-                <div><input type='button' value='9' onClick={this.handleClick}/></div>
-                <div><input type='button' value='8' onClick={this.handleClick}/></div>
-                <div><input type='button' value='7' onClick={this.handleClick}/></div>
-                <div><input type='button' value='6' onClick={this.handleClick}/></div>
-                <div><input type='button' value='5' onClick={this.handleClick}/></div>
-                <div><input type='button' value='4' onClick={this.handleClick}/></div>
-                <div><input type='button' value='3' onClick={this.handleClick}/></div>
-                <div><input type='button' value='2' onClick={this.handleClick}/></div>
-                <div><input type='button' value='1' onClick={this.handleClick}/></div>
-                <div><input type='button' value='0' onClick={this.handleClick}/></div>
-                <div><input type='button' value='+' onClick={this.handleClick}/></div>
-                <div><input type='button' value='-' onClick={this.handleClick}/></div>
-                <div><input type='button' value='*' onClick={this.handleClick}/></div>
-                <div><input type='button' value='/' onClick={this.handleClick}/></div>
-                <div><input type='button' value='CE' onClick={this.handleClick}/></div>
-                <div><input type='button' value='=' onClick={this.handleClick}/></div>
+               {items}
             </div>
         );
     }
