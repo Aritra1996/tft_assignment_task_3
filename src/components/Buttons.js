d9f0ff6dd92a56e369a0eb199f3d486ba065d369
Aritra1996 <arakshit.engineer@gmail.com>
2022-01-20 20:39:01 +0530

    Refactored codebase

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -61,7 +61,6 @@ export class Buttons extends React.Component {
                         ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
                         ['CE', '=']]
 
-        console.log(buttons[0])
         let items = buttons[0].map((operator) =>
             <div key={operator}><input type='button' value={operator} onClick={this.handleOperators}/></div>
         )
